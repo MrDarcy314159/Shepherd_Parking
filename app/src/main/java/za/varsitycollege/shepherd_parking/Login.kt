a9e91037d00006a15028b09676b26941515c6639
Ross Hartigan <rosshartigan@gmail.com>
2024-09-17 11:37:27 +0200

    adding branch

--- app/src/main/java/za/varsitycollege/shepherd_parking/Login.kt
+++ app/src/main/java/za/varsitycollege/shepherd_parking/Login.kt
@@ -25,7 +25,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
 
-// You suck
+// Testing Room-DB Branch
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginPage(navController: NavController) {
